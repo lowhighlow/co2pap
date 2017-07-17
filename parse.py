83617861144f6284e074ec8b053a9d0a98cfcd84
Gustav Berth <gustav.berth@mnet-online.de>
2017-07-17 15:30:22 +0200

    Check for nan implemented

--- parse.py
+++ parse.py
@@ -1,13 +1,9 @@
+#imports
 import numpy as np
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 
-<<<<<<< master
-csv = pd.read_csv('/home/gustav/PowerConsumptionData/n004', sep='\s*,\s*', header=0, encoding='ascii', engine='python')
-df = pd.DataFrame(csv)
-plt.plot(df['Time'])
-plt.show()
-=======
 #functions
 def toStr(n):
 
@@ -21,6 +17,21 @@ def toStr(n):
 def get(n):
     return df[n - 4]
 
+def dropNan(c):
+    c = c['Time']
+    k = 0
+    while True:
+        try:
+            if math.isnan(c[k]):
+                c.drop(k)
+                if math.isnan(c[k]) == false:
+                    c.drop(k)
+            k += 1
+        except KeyError:
+            break
+    
+        
+
 #main code
 csv = pd.read_csv('/home/gustav/PowerConsumptionData/n004', sep='\s*,\s*', header=0, encoding='ascii', engine='python')
 df = [pd.DataFrame(csv)]
@@ -28,11 +39,11 @@ l = 5
 while True:
     try:
         currentDF = pd.DataFrame(pd.read_csv('/home/gustav/PowerConsumptionData/n' + toStr(l), sep='\s*,\s*', header=0, encoding='ascii', engine='python'))
+        dropNan(currentDF)
         df.append(currentDF)
         print('Loaded n' + toStr(l))
         l += 1
     except FileNotFoundError:
         break
 
->>>>>>> local
 
