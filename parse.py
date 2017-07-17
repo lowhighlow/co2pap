bec1476837e950558d444efcbdb407a43c0206f5
Gustav Berth <gustav.berth@mnet-online.de>
2017-07-17 13:51:10 +0200

    Draws plaot('Time')

--- parse.py
+++ parse.py
@@ -1,2 +1,9 @@
-csv  = pd.read_csv('/home/gustav/PowerConsumptionData/n004', sep='\s*,\s*')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+csv = pd.read_csv('/home/gustav/PowerConsumptionData/n004', sep='\s*,\s*', header=0, encoding='ascii', engine='python')
+df = pd.DataFrame(csv)
+plt.plot(df['Time'])
+plt.show()
 
